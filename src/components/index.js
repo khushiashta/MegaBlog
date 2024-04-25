4d15a311e69a3a131a98eb2ce7b4fc8248eb2a64
Khushi Ashta <khushiashta@192.168.1.2>
2024-04-25 19:05:06 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Select from "./Select.jsx";
+import Header from "./header/header";
+import Footer from "./footer/footer";
+import Container from "./container/Container.jsx";
+import Logo from "./Logo.jsx";
+import LogoutBtn from "./header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm.jsx";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input.jsx";
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout, 
+    Input,
+    Select,
+}
